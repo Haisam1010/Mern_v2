cde8a1b6df40682ad8926a92c1c01ed7a047f7b8
Haisam <mohamedhaisam199@gmail.com>
2023-11-08 12:11:13 +0530

    reinitialized

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,50 @@
+import {Outlet} from 'react-router-dom'
+import Wrapper from "../wrappers/DashBoard"
+import { SmallSideBar,BigSideBar,NavBar } from '../components'
+import { createContext, useContext, useState } from 'react'
+
+const DashBoardContext = createContext()
+
+const Dashboard = () => {
+
+  const user = ['mohamed']
+
+  const [showSidebar,setShowSidebar] = useState(false)
+  const [isDarkTheme,setIsDarkTheme] = useState()
+
+  const toggleDarkTheme = () => {
+    console.log('toggle dark theme')
+  }
+
+
+  const toggleSidebar = () => { 
+    setShowSidebar(!showSidebar)
+    console.log('toggle')
+  }
+
+
+  const logoutUser = async () => {
+    console.log('logout user')
+  }
+
+  return( 
+    <DashBoardContext.Provider value={{user,showSidebar,isDarkTheme,toggleSidebar,toggleDarkTheme,logoutUser}}>
+       <Wrapper>
+  <main className='dashboard'>
+    <SmallSideBar />
+    <BigSideBar />
+
+    <div>
+      <NavBar />
+      <div className='dashboard-page'>
+        <Outlet />
+      </div>
+    </div>
+  </main>
+
+  </Wrapper>
+    </DashBoardContext.Provider>
+ )
+}
+export  const useDashBoardContext = () => useContext(DashBoardContext)
+export default Dashboard
